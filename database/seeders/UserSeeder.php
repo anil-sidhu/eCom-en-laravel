10d2fe240bf6870125602ada3438c8da7fcf5885
anil <webanilsidhu@gmail.com>
2020-10-09 19:19:02 +0530

    db config done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'peter parker',
+            'email'=>'peter@parker.com',
+            'password'=>Hash::make('12345')
+        ]);
+    }
+}
