e212bcdaa73ec59e456bf49ca1e501ee9f43413b
anil <webanilsidhu@gmail.com>
2020-10-10 23:46:04 +0530

    done with product table

---
+++ database/migrations/2020_10_10_173100_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("price");
+            $table->string("category");
+            $table->string("description");
+            $table->string("gallery");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            [
+                'name'=>'Oppo mobile',
+                "price"=>"300",
+                "description"=>"A smartphone with 8gb ram and much more feature",
+                "category"=>"mobile",
+                "gallery"=>"https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBOPPO-A52-6-GFUTU6297453D3D253C/1592019058170_0..png"
+            ],
+            [
+                'name'=>'Panasonic Tv',
+                "price"=>"400",
+                "description"=>"A smart tv with much more feature",
+                "category"=>"tv",
+                "gallery"=>"https://i.gadgets360cdn.com/products/televisions/large/1548154685_832_panasonic_32-inch-lcd-full-hd-tv-th-l32u20.jpg"
+            ],
+            [
+                'name'=>'Soni Tv',
+                "price"=>"500",
+                "description"=>"A tv with much more feature",
+                "category"=>"tv",
+                "gallery"=>"https://4.imimg.com/data4/PM/KH/MY-34794816/lcd-500x500.png"
+            ],
+            [
+                'name'=>'LG fridge',
+                "price"=>"200",
+                "description"=>"A fridge with much more feature",
+                "category"=>"fridge",
+                "gallery"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFx-2-wTOcfr5at01ojZWduXEm5cZ-sRYPJA&usqp=CAU"
+             ]
+        ]);
+    }
+}
