fa313a1c42bddf736a1c193ce37a2032a16a41f4
anil <webanilsidhu@gmail.com>
2020-10-17 00:06:39 +0530

    logout and login done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,10 @@
 <?php 
 use App\Http\Controllers\ProductController;
-$total= ProductController::cartItem();
+$total=0;
+if(Session::has('user'))
+{
+  $total= ProductController::cartItem();
+}
 
 ?>
 <nav class="navbar navbar-default">
@@ -13,7 +17,7 @@ $total= ProductController::cartItem();
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Brand</a>
+        <a class="navbar-brand" href="/">E-Comm</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
@@ -30,7 +34,17 @@ $total= ProductController::cartItem();
         </form>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#">cart({{$total}})</a></li>
-          
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+
 Route::post("/login",[UserController::class,'login']);
 Route::get("/",[ProductController::class,'index']);
 Route::get("detail/{id}",[ProductController::class,'detail']);
